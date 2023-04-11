70b33a62dd44dbbc31c228bfea7e7646da840e48
Alexander <113671236+REDALXA@users.noreply.github.com>
2023-04-11 20:45:09 +0700

    Add files via upload

---
+++ parameter.py
@@ -0,0 +1,6 @@
+mass = 1000
+Energy_sub = 0
+Energy_bra = 0
+aux = 500
+ess_max = 30000
+station = [1000, 1800, 3200]
---
+++ simulation.py
@@ -0,0 +1,18 @@
+from event import *
+
+timestep = 0
+
+def accelerate(Train, station): # function to decide train acceleration
+    if Train.position < station[0] :
+        
+
+def simulation():
+    accelerate()
+    event()
+    timestep += 1
+    if timestep >= 1000 :
+        printsituation() ##
+
+
+def printsituation(Train, ESS):
+    print("Train position is in ")
